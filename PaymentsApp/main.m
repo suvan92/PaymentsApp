c8d2680728d3ae1afff0333287e795ad332809f6
Suvan <suvanramani@gmail.com>
2016-11-04 11:06:30 -0700

    Initial Commit

---
+++ PaymentsApp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PaymentsApp
+//
+//  Created by Suvan Ramani on 2016-11-04.
+//  Copyright © 2016 suvanr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
